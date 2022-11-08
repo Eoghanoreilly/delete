50476abb365c3256e5c6dd02574b632e7e11c372
Eoghan OReilly <eoghan@vestego.com>
2022-11-08 17:53:28 +0100

    Initial Commit

---
+++ Vestego/VestegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VestegoApp.swift
+//  Vestego
+//
+//  Created by Eoghan O Reilly on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct VestegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
